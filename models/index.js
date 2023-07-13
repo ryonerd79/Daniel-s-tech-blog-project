8d17795b076676d9a71251667685404ad65623be
Muramori71 <dannyroh2500@gmail.com>
2023-07-13 10:33:45 -0700

    created models folder and files

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+   foreignKey: 'user_id' 
+})
+
+module.exports = { User, Post, Comment};
